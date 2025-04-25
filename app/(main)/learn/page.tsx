8105987f595650920baa0472bab486da3aaa500a
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-04-25 16:03:25 -0500

    Courses Page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,12 +2,21 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+    const userProgressData = getUserProgress();
+    const [userProgress] = await Promise.all([userProgressData]);
+
+    if(!userProgress || !userProgress.activeCourse) {
+        redirect("/courses")
+    }
+
     return ( 
         <div className="flex flex-row-reverse gap-[48px] px-6">{/*switch the feeds maybe, and remove flex-row-reverse*/}
             <StickyWrapper>
-                <UserProgress activeCourse={{title: "Spanish", imageSrc: "duoMascot.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
+                <UserProgress activeCourse={{title: "Spanish", imageSrc: "/es.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
             </StickyWrapper>
             <FeedWrapper>
                 <Header title="Spanish" />
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from '@clerk/nextjs'
+import { Toaster } from "@/components/ui/sonner";
 
 const font = Nunito({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body
           className={`${font.variable} antialiased`}
         >
+          <Toaster/>
           {children}
         </body>
       </html>
